144f952737aaf71b9ad3430eb04dd40bff1345c4
Tim A. Conner <timaconner1@gmail.com>
2018-04-30 16:19:24 -0500

    Change note_date timestamp data type to date.

--- sequelize/models/note_date.js
+++ sequelize/models/note_date.js
@@ -1,10 +1,10 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Note_Date = sequelize.define('Note_Date', {
-    timestamp: DataTypes.TIMESTAMP
+    timestamp: DataTypes.DATE
   }, {});
   Note_Date.associate = function(models) {
-    Note_Date.hasOne(models.Note, {
+    Note_date.hasOne(models.Note, {
       foreignKey: 'note_id'
     });
   };
