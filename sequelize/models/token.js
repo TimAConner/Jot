6159f22f229bf195bf98f64b86a2abb772c190dc
Tim A. Conner <timaconner1@gmail.com>
2018-05-01 16:33:27 -0500

    Add expiration date to token.

--- sequelize/models/token.js
+++ sequelize/models/token.js
@@ -2,7 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   var Token = sequelize.define('Token', {
     value: DataTypes.STRING,
-    user_email: DataTypes.STRING
+    user_email: DataTypes.STRING,
+    expire_date: DataTypes.DATE
   }, { tableName: 'tokens', timestamps: false });
   Token.associate = function(models) {
     // associations can be defined here
