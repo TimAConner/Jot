70bf1f05d3ab594046c1cf68d5c9e8f7162d8be0
Tim A. Conner <timaconner1@gmail.com>
2018-05-03 11:13:09 -0500

    Add watson keyword analyzer function 'getKeywords'.

---
+++ server/watson.js
@@ -0,0 +1,21 @@
+'use strict';
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+const watsonCredentials = require('./config/watsonConfig.js');
+const watson = new NaturalLanguageUnderstandingV1(watsonCredentials);
+
+module.exports.getKeywords = noteText => {
+  return new Promise((resolve, reject) => {
+    watson.analyze(
+      {
+        html: noteText,
+        features: {
+          keywords: {},
+        },
+      },
+      (err, response) => {
+        if (err) return reject(err);
+        resolve(console.log(JSON.stringify(response)))
+      },
+    );
+  });
+};
