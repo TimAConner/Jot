fb0c72351b03f251ac1122be607bd4bf231e96c8
Tim A. Conner <timaconner1@gmail.com>
2018-05-09 15:09:42 -0500

    Modify Note component button and date styling.

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Note = ({ noteId, keywords, date, text, viewNote, deleteNote}) => (
   <div className='note' id={noteId} onClick={() => viewNote(noteId)}>
-    <button className='note__delete' onClick={() => deleteNote(noteId)}>Delete</button>
     <h2 className='note__keywords'>{keywords}</h2>
-    <h3 className='note__date'>{date}</h3>
-    <p className='note__text'>{text}</p>
+    <p className='note__date'>{date}</p>
+    <p className='note__text'>{text}</p> 
+    <button className='note__delete' onClick={() => deleteNote(noteId)}>Delete</button>
+    {/* Modify to show only first x amount of text?  Possibly set that in the css. */}
   </div>
 );
 
