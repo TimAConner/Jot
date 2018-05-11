5f81f36ff41beb9b35a28b6ae6b5edfe6ceffd87
Tim A. Conner <timaconner1@gmail.com>
2018-05-11 11:35:51 -0500

    Implement basic search functionality for keywords.

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -11,11 +11,13 @@ class NoteList extends React.Component {
 
   constructor(props) {
     super(props);
-    console.log('state', this.state);
-    console.log('props', this.props);
 
-    this.viewNote = this.viewNote.bind(this);
-    this.deleteNote = this.deleteNote.bind(this);
+    this.state = {
+      searchTerm: "",
+    };
+
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.keywordMatch = this.keywordMatch.bind(this);
   }
 
   viewNote(note) {
@@ -44,10 +46,26 @@ class NoteList extends React.Component {
     }
   }
 
+  keywordMatch({ keyword }) {
+    console.log(this.state);
+    const keywordMatch = new RegExp(`.*${this.state.searchTerm}.*`, 'i');
+    return keyword.match(keywordMatch);
+  }
+
   generateList() {
     switch (this.props.sortBy) {
       case 'notes': {
-        return this.props.notes.map(({ id, Keywords: keywords, Note_Dates: [{ edit_date: date }], text }) => {
+
+        // filter on search term and then map information to Note component
+        return this.props.notes.filter(note => {
+          if (this.state.searchTerm.trim() === '') {
+            return note;
+          }
+
+          if (note.Keywords.some(this.keywordMatch)) {
+            return note;
+          }
+        }).map(({ id, Keywords: keywords, Note_Dates: [{ edit_date: date }], text }) => {
           return (<Note
             noteId={id}
             keywords={keywords.length > 0 ? keywords.map(keywordObj => keywordObj.keyword).reduce((acc, cv) => acc + ", " + cv) : []}
@@ -95,18 +113,9 @@ class NoteList extends React.Component {
     }
   }
 
-  // ote
-  // :
-  // {text: "This is another test with a strange word.  Lets ed…it today.  There should be another edit date now.", user_id: 1, Keywords: Array(5)}
-  // edit_date
-  // :
-  // "2018-05-11T14:36:02.860Z"
-  // id
-  // :
-  // 8
-  // note_id
-  // :
-  // 2
+  handleSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
 
   render() {
     return (
@@ -115,7 +124,9 @@ class NoteList extends React.Component {
         {this.props.saving ? <Loader
           text='Saving'
         /> : null}
-        <input type='text' placeholder='Search...' />
+
+        <input type='text' value={this.state.searchTerm} onChange={this.handleSearchChange} placeholder='Search...' />
+
         <button onClick={() => this.props.viewAllNotes()}>Sort by Note</button>
         <button onClick={() => this.props.viewNotesByDates()}>Sort by Edit Date</button>
         <button>Sort by Week</button>
