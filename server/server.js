00fa524ec487906f55281b6ab43aed706743206a
Tim A. Conner <timaconner1@gmail.com>
2018-05-14 10:25:35 -0500

    Add patch as an allowed header on the server.

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 
 app.options("/*", function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, dataType, contentType');
   res.status(200).json();
 });
